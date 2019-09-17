9305721633a1032d1a6563b000c07a81a3743086
CodyThomasCrump <codytcrump@gmail.com>
2019-09-17 14:48:26 -0500

    Finished building the clock and links

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,11 @@ function showTime() {
     var hour = date.getHours(); // 0-23
     var minute = date.getMinutes(); // 0-59
     var seconds = date.getSeconds(); // 0-59
+    var month = date.getMonth();
+    var day = date.getDate();
+    var year = date.getFullYear(); 
     var period = "AM"; // We're going to assume that we're in AM to start
-
+    
     // We need to make sure the hour is properly represented in 12-hr time, and we also need to determine whether we should display AM or PM
 
     // With our hours being between 0 and 23, 0 would equal 12AM and everything 12 0r above is a PM hour
@@ -38,10 +41,17 @@ function showTime() {
     
     // String to store the current time
     var time = hour + ":" + minute + ":" + seconds + " " + period;
+    var currentDate = month + "/" + day + "/" + year;
 
     document.getElementById("clockDisplay").innerText = time;
+    document.getElementById("dateDisplay").innerText = currentDate;
 
     setTimeout(showTime, 1000); // Schedule the showTime function to be called after 1000 ms (1 second)
 }
 
 showTime();
+
+// JavaScript Clock Challenges:
+// 1. Add a different font (of your choice) for your clock to use.
+// 2. Display the current date below the time. For example: Today's date is: Tuesday, September 17th. *Hint - hours, minutes, and seconds aren't the only thing we can pull from a date object*
+// 3. Add a header to your clock displaying your name, GitHub link, and LinkedIn profile link.
